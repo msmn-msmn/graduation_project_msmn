ae8713272dbf0cfe03d0d677a33ab95a2e9cfbb0
msmn-msmn <m56998673@gmail.com>
2025-08-29 02:35:18 +0900

    Taskモデルルーティング設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,10 @@ Rails.application.routes.draw do
   }
 
   resources :users, only: %i[index show edit update destroy] # new,createはDeviseから提供される
+  resources :tasks  # 全アクション使用のため省略形
 
   root to: "static_pages#index"
   get "static_pages/index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -21,6 +21,4 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
