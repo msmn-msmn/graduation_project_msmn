7ba2dcebe15d407992def139298f7bdc87b7d1cf
msmn-msmn <m56998673@gmail.com>
2025-08-30 05:51:01 +0900

    コンフリクト修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ]) # ユーザー登録(sign_up)時に username を許容する
     devise_parameter_sanitizer.permit(:account_update, keys: [ :username ]) # ユーザーのプロフィール更新(account_update)時にも username を 許容する
   end
+
+
+
+   # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource)
+    # ログイン前にアクセスしようとしたページがあれば優先
+    stored_location_for(resource) || tasks_path
+  end
 end
