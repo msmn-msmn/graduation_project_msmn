fa3dbaaa32394f069b5109c9b3842c9001bbfe63
msmn-msmn <m56998673@gmail.com>
2025-08-30 05:46:16 +0900

    ログイン項目にusername追加完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :configure_permitted_parameters, if: :devise_controller? #Deviseのコントローラであるときだけ実行する
+  before_action :configure_permitted_parameters, if: :devise_controller? # Deviseのコントローラであるときだけ実行する
 
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username]) #ユーザー登録(sign_up)時に username を許容する
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username]) #ユーザーのプロフィール更新(account_update)時にも username を 許容する
-  end  
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ]) # ユーザー登録(sign_up)時に username を許容する
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :username ]) # ユーザーのプロフィール更新(account_update)時にも username を 許容する
+  end
 end
