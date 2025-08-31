b3433a7b70c8a4064ae8e4fe80b375e7f958aad9
msmn-msmn <m56998673@gmail.com>
2025-09-01 02:55:47 +0900

    SubTasksコントローラーの生成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "sub_tasks/edit"
+  get "sub_tasks/update"
+  get "sub_tasks/destroy"
+  get "sub_tasks/complete"
+  get "sub_tasks/restart"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users,
   path: "",
