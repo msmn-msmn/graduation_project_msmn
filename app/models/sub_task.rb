48e68d08d0ae4d4147dc642d9407dda21ce47b6c
msmn-msmn <m56998673@gmail.com>
2025-08-31 17:34:21 +0900

    SubTasksテーブル生成完了

---
+++ app/models/sub_task.rb
@@ -0,0 +1,4 @@
+class SubTask < ApplicationRecord
+  belongs_to :user
+  belongs_to :task
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_30_053532) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_31_075412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sub_tasks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "task_id", null: false
+    t.string "name", null: false
+    t.string "step_name"
+    t.integer "status", default: 0, null: false                    # 進行管理enum用
+    t.integer "priority", default: 0, null: false                  # 優先度enum用
+    t.integer "sub_work_time"                                      # 実際作業時間
+    t.datetime "sub_due_date"                                      # 小タスクの締切日
+    t.datetime "completed_at"                                      # 小タスクの完了日
+    t.datetime "step_due_date"                                     # 小タスクのステップの締切日
+    t.datetime "step_completed_at"                                 # 小タスクのステップの完了日
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["priority"], name: "index_sub_tasks_on_priority"      # 優先度ソート用
+    t.index ["status"], name: "index_sub_tasks_on_status"          # ステータス検索用
+    t.index ["task_id"], name: "index_sub_tasks_on_task_id"
+    t.index ["user_id"], name: "index_sub_tasks_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
     t.bigint "user_id", null: false
-    t.text "description_for_ai"
+    t.text "description_for_ai"                                    # AI用説明文
     t.text "user_memo"
-    t.integer "status", default: 0, null: false
-    t.integer "priority", default: 0, null: false
-    t.integer "daily_task_time", null: false
-    t.integer "estimate_min_days", null: false
-    t.integer "estimate_normal_days", null: false
-    t.integer "estimate_max_days", null: false
-    t.integer "calculated_estimated_days"
-    t.integer "work_time"
-    t.datetime "due_date"
-    t.datetime "completed_at"
+    t.integer "status", default: 0, null: false                    # 進行管理enum用
+    t.integer "priority", default: 0, null: false                  # 優先順位enum用
+    t.integer "daily_task_time", null: false                       # 1日のタスクに使える時間
+    t.integer "estimate_min_days", null: false                     # 完了見積最短
+    t.integer "estimate_normal_days", null: false                  # 完了見積普通
+    t.integer "estimate_max_days", null: false                     # 完了見積最大
+    t.integer "calculated_estimated_days"                          # 算出見積時間
+    t.integer "work_time"                                          # 実際作業時間
+    t.datetime "due_date"                                          # タスクの締切日
+    t.datetime "completed_at"                                      # タスクの完了日
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
@@ -54,5 +74,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_30_053532) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "sub_tasks", "tasks"
+  add_foreign_key "sub_tasks", "users"
   add_foreign_key "tasks", "users"
 end
