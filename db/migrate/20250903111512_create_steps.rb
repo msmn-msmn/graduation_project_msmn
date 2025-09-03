69c697923fca14df52fc2ba5233d672b7b690f03
msmn-msmn <m56998673@gmail.com>
2025-09-03 21:10:45 +0900

    stepsテーブル作成

---
+++ db/migrate/20250903111512_create_steps.rb
@@ -0,0 +1,17 @@
+class CreateSteps < ActiveRecord::Migration[7.2]
+  def change
+    create_table :steps do |t|
+      t.references :sub_task, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false
+      t.integer :status, default: 0, null: false
+      t.integer :position, default: 0, null: false
+      t.datetime :due_date
+      t.datetime :completed_at
+      t.timestamps
+    end
+
+    add_index :steps, [:sub_task_id, :position], unique: true
+    add_index :steps, :status
+  end
+end
