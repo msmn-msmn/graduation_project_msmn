2195ad590cdf8b737c392866fcd3f9d4f080671e
msmn-msmn <m56998673@gmail.com>
2025-10-23 23:46:44 +0900

    メールアドレスの変更機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :daily_available_time)
+    params.require(:user).permit(:username, :daily_available_time, :email)
   end
 end
