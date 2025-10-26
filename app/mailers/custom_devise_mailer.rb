1532b5309a44bdc0fab213728bb706fe21f1fdd7
msmn-msmn <m56998673@gmail.com>
2025-10-26 23:35:39 +0900

    reconfirmation_instructionsテンプレートファイルの作成

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -2,7 +2,7 @@ class CustomDeviseMailer  < Devise::Mailer
   # 新規登録/変更 どちらでも呼ばれる
   def confirmation_instructions(record, token, opts = {})
     if record.respond_to?(:pending_reconfirmation?) && record.pending_reconfirmation?
-      # プロフィールのメールアドレス変更フロー
+      # プロフィールからのメールアドレス変更の確認
       opts[:template_name] = 'reconfirmation_instructions'     # 変更確認用テンプレート
       opts[:subject]       = I18n.t('devise.mailer.reconfirmation_instructions.subject')
     else
