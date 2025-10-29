e641cb785c06c80a427829625edabd9643657820
msmn-msmn <m56998673@gmail.com>
2025-10-29 23:55:50 +0900

    ユーザーのメールアドレス変更メール未確認判定メソッド追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # ユーザーのメールアドレス変更メール未確認判定メソッド
+  def show_pending_email_banner?
+    user_signed_in? &&
+    current_user.respond_to?(:pending_reconfirmation?) &&
+    current_user.pending_reconfirmation? &&
+    current_user.confirmation_period_valid?
+  end
 end
