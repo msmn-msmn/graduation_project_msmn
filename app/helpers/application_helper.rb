5ab42110530b3b99271027ef279d5f1bf62672e1
msmn-msmn <m56998673@gmail.com>
2025-10-30 03:08:55 +0900

    reconfirmation_instructions.html.erb内容修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,10 @@ module ApplicationHelper
   # ユーザーのメールアドレス変更メール未確認判定メソッド
   def show_pending_email_banner?
     user_signed_in? &&
-    current_user.respond_to?(:pending_reconfirmation?) &&
-    current_user.pending_reconfirmation? &&
-    current_user.confirmation_period_valid?
+      current_user.respond_to?(:pending_reconfirmation?) &&
+      current_user.pending_reconfirmation? &&
+      current_user.respond_to?(:confirmation_period_valid?) &&
+      current_user.confirmation_period_valid?
+      current_user.unconfirmed_email.present?
   end
 end
