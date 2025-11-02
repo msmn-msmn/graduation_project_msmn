125086cc84fe4e0d97d5cbc282833996887698c5
msmn-msmn <m56998673@gmail.com>
2025-11-02 23:40:05 +0900

    def show_pending_email_banner?修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,17 @@
 module ApplicationHelper
   # ユーザーのメールアドレス変更メール未確認判定メソッド
   def show_pending_email_banner?
-    user_signed_in? &&
-      current_user.respond_to?(:pending_reconfirmation?) &&
-      current_user.pending_reconfirmation? &&
-      current_user.respond_to?(:confirmation_period_valid?) &&
-      current_user.confirmation_period_valid?
-      current_user.unconfirmed_email.present?
+    return false unless user_signed_in?
+    user = current_user
+
+    # Deviseのreconfirmable: メール変更中か？
+    return false unless user.respond_to?(:pending_reconfirmation?) &&
+                        user.pending_reconfirmation?
+
+    # トークンの有効期間内か？（リンクの有効性）
+    return false unless user.respond_to?(:confirmation_period_expired?) &&
+                        user.confirmation_period_expired?
+    current_user.unconfirmed_email.present?
   end
 
   # メール送信のレート制限（連打対策）
