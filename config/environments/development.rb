3598b2cd11df46dc0e26d654b53a7c03f047254a
msmn-msmn <m56998673@gmail.com>
2025-11-25 01:13:13 +0900

    開発環境アクセス設定変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,10 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
+  # URLヘルパー(_url)やTurbo/ActionMailerが使う既定値
+  config.action_controller.default_url_options = { host: "localhost", port: 3000, protocol: "http" }
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.force_ssl = false
 
   config.action_mailer.delivery_method = :letter_opener_web
 
